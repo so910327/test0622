781fc31885e43a0dc85f9aac189bb4deb7bbe61c
TingRu.Chen <so910327@gmail.com>
2017-06-22 15:06:53 +0800

    try

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var restify = require('restify');
- 
+
+
+//server
 var server = restify.createServer({
   name: 'myapp',
   version: '1.0.0'
@@ -7,12 +9,27 @@ var server = restify.createServer({
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
- 
+
 server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
+  console.log(add(1,2));
   return next();
 });
- 
+
+function add(n1, n2){
+	return n1+n2;
+}
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
+});
+
+//client
+var client = restify.createJsonClient({
+  url: 'http://localhost:8080',
+  version: '~1.0'
+});
+
+client.get('/echo/this%20is%20just%20a%20test', function (err, req, res, obj) {
+  if(err) console.log("An error ocurred:", err);
+  else console.log('Server returned: %j', obj);
 });
---
+++ test/test.js
@@ -0,0 +1,36 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../app');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('connect', function() {
+  it('fails, as expected', function() { // <= Pass in done callback
+    chai.request('http://localhost:8080')
+        .get('/fail')
+        .end(function(err, res) {
+          //console.log(res);
+          expect(res).to.have.status(404);
+          //done();                               // <= Call done to signal callback end
+        });
+  });
+  it('succeeds silently!', function() {   // <= No done callback
+    chai.request('http://localhost:8080')
+        .get('/')
+        .end(function(err, res) {
+          expect(res).to.have.status(123);    // <= Test completes before this runs
+        });
+    });
+
+});
+
+
+describe('assert', function () {
+  var assert = chai.assert;
+
+  it('assert', function () {
+    assert.equal(4, 1+3, 'addSum');
+
+  });
+});
